40490e433285b7814c6481dd7e7cc02c8e1b4084
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-02-01 10:31:44 +0000

    - Finished question
    - Added files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+//Implement a program to draw a border of stars around a sting of text.
+
+int main() {
+    string userInput;
+
+    cout << "Enter your text:";
+    cin >> userInput;
+
+    string border = string(userInput.length() + 4, '*');
+
+    cout << border << endl;
+    cout << "* " << userInput << " *" << endl;
+    cout << border << endl;
+
+    return 0;
+}
